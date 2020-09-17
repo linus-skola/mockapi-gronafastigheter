89b586e8b573e3e0ffd9db13cd6dab77cc982ea8
Linus Örnborg <linus.skola@ornborg.se>
2020-09-17 11:33:43 +0200

    removed nulls

--- estates.php
+++ estates.php
@@ -18,7 +18,7 @@ class RealEstate {
         $obj1->Id = 1;
         $obj1->Title = "Big House";
         $obj1->SellingPrice = 1100400;
-        $obj1->RentingPrice = null;
+        $obj1->RentingPrice = 0;
         $obj1->CanBeSold = true;
         $obj1->CanBeRented = false;
     
@@ -33,7 +33,7 @@ class RealEstate {
         $obj3 = new RealEstateData;
         $obj3->Id = 3;
         $obj3->Title = "Medium Apartment";
-        $obj3->SellingPrice = null;
+        $obj3->SellingPrice = 0;
         $obj3->RentingPrice = 13500;
         $obj3->CanBeSold = false;
         $obj3->CanBeRented = true;
